611bb155b9bdfe1e357fe03b11ea2fdc3bb524d7
xiaobao <xiaobaocrazy@aliyun.com>
2019-03-19 16:46:50 +0800

    idea update1

--- src/main/java/com/itxiaobao/BaseController.java
+++ src/main/java/com/itxiaobao/BaseController.java
@@ -3,5 +3,6 @@ package com.itxiaobao;
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("idea 的测试");
+        System.out.println("测试11");
     }
 }
