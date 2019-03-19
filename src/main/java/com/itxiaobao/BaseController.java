642040e117ae8a822e24b802d6016c8c59e5bcdf
xiaobao <xiaobaocrazy@aliyun.com>
2019-03-19 17:38:20 +0800

    left

--- src/main/java/com/itxiaobao/BaseController.java
+++ src/main/java/com/itxiaobao/BaseController.java
@@ -4,5 +4,6 @@ public class BaseController {
     public static void main(String[] args) {
         System.out.println("idea 的测试");
         System.out.println("测试11");
+        System.out.println("lllllllll");
     }
 }
